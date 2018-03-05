59524ef020b17e7884dbc571396164a51467a049
neojamal747 <neojamal747@gmail.com>
2018-03-05 11:48:27 -0500

    https://github.com/neojamal747/Friend_Finder.git

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+	{
+		name: "NavJ",
+		photoURL: "https://scontent.fewr1-1.fna.fbcdn.net/v/t31.0-8/16300052_10154030130387531_7503739427793013329_o.jpg?oh=e472e7f0de92151ba97beee6195457f8&oe=5B0A88CBg",
+		scores:[
+		    5,
+		    1,
+		    4,
+		    4,
+		    5,
+		    1,
+		    2,
+		    5,
+		    4,
+		    1
+		]
+	},
+];
+
+module.exports = friendsArray;
